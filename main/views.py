3c75f2a225c1ff233fe1e13fdf409b07219ea8e3
TusharMEH <49725000+TusharMEH@users.noreply.github.com>
2019-06-14 21:30:56 +0530

    all files are finally saved

--- main/views.py
+++ main/views.py
@@ -9,8 +9,7 @@ from .models import Tutorial, TutorialCategory, TutorialSeries
 
 # Create your views here.
 def single_slug(request, single_slug):
-    # first check to see if the url is in categories.
-
+    
     categories = [c.category_slug for c in TutorialCategory.objects.all()]
     if single_slug in categories:
         matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
@@ -34,29 +33,33 @@ def single_slug(request, single_slug):
                       context={"tutorial": this_tutorial,
                                "sidebar": tutorials_from_series,
                                "this_tut_idx": this_tutorial_idx})   
-
-    return HttpResponse(f"{single_slug} this is a tutorial")
+    
+    return HttpResponse(f"{single_slug} this is nothing to our knowledge")
 
 def homepage(request):
 	return render(request,
 				  "main/categories.html",
 				  {"categories":TutorialCategory.objects.all})
-def register(request):
-	if request.method=="POST":
-		form=NewUserForm(request.POST)
-		if form.is_valid():
-			user=form.save()
-			username=form.cleaned_data.get("username")
-			messages.success(request,f"you are successfully logged in :{username}")
-			login(request,user)
-			return redirect("main:homepage")
-		else:
-			for msg in form.error_messages:
-				messages.error(request,f"{msg}:{form.error_messages[msg]}")
-			return render(request,"main/register.html",{'form':form})
 
-	form=NewUserForm
-	return render(request,"main/register.html",{'form':form})
+def register(request):
+  if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+          user = form.save()
+          username = form.cleaned_data.get('username')
+          messages.success(request,f"successfully logged in as {username}")
+          login(request, user)
+          return redirect("main:homepage")            
+        else:
+          for msg in form.error_messages:
+            messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+
+  form = NewUserForm
+  return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
+	  
 
 def logout_request(request):
  	logout(request)
